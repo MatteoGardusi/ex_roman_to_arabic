3cf49b7309e353db1075b4511aa1c6871990cab1
Matteo Gardusi <matteo.gardusigmail.com>
2022-11-28 14:50:21 +0100

    C'è la funzione che verifica la legalità della stringa in ingresso

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def verifica_stringa_legale(data_in: str) -> str:
+    """
+    Riceve una stringa e verifica se i caratteri
+    sono legali
+    :param data_in: stringa da controllare
+    :return:
+    """
+    legal_symbol = {'I', 'V', 'X', 'L', 'C', 'D', 'M'}
+    data_in = data_in.upper()
+    for char in data_in:
+        if char not in legal_symbol:
+            return "Hai inserito un carattere illegale"
+    return data_in
+
+
 def convert_roman_to_arabic(roman: str) -> str:
     """
     convert a string containing a number in roman coding to a
@@ -5,5 +20,9 @@ def convert_roman_to_arabic(roman: str) -> str:
     :param roman: string to be converted
     :return: converted string
     """
-    # inserisci qui il tuo codice
-    pass
+
+    return str(arabic)
+
+
+roman_string = verifica_stringa_legale(input('Scrivi un numero romano: '))
+print(roman_string)
